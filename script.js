4d0ae93685299975aedf8404e67b58f9a188c617
YGWoong <100560289+YGWoong@users.noreply.github.com>
2026-05-24 19:28:46 +0900

    Update script.js

--- script.js
+++ script.js
@@ -780,4 +780,19 @@
   } else {
     init();
   }
+
+  document.addEventListener('touchmove', function (event) {
+    if (event.scale !== undefined && event.scale !== 1) {
+      event.preventDefault();
+    }
+  }, { passive: false });
+
+  let lastTouchEnd = 0;
+  document.addEventListener('touchend', function (event) {
+    const now = (new Date()).getTime();
+    if (now - lastTouchEnd <= 300) {
+      event.preventDefault();
+    }
+    lastTouchEnd = now;
+  }, false);
 })();
