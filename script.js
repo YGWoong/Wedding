fcc9506f30f8a16fcc9cd3235cdc078e3226cea9
woong <mobubso123@sjsofttech.co.kr>
2026-07-12 23:36:48 +0900

    img change

--- script.js
+++ script.js
@@ -109,7 +109,7 @@
         };
         setMeta('property', 'og:title', m.title);
         setMeta('property', 'og:description', m.description);
-        setMeta('property', 'og:image', 'images/mt/1.jpg');
+        setMeta('property', 'og:image', 'images/og/1.jpg');
         setMeta('name', 'description', m.description);
     }
 
